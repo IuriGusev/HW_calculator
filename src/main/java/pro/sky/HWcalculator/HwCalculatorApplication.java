c4995949901c32b963723c4cfb89d38c8f3d95cc
iurii_gusev <gusevyuv@gmail.com>
2023-08-19 13:35:38 -0400

    first

---
+++ src/main/java/pro/sky/HWcalculator/HwCalculatorApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.HWcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculatorApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HwCalculatorApplication.class, args);
+	}
+
+}
